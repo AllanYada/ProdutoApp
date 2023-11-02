28ec884dcbfa02111457f7760482fdaa389b210f
AllanYada <allan.yada02@hotmail.com>
2023-11-02 15:45:14 -0300

    Adicionando dialog para informar error no servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EditProdutoComponent } from './pages/edit-produto/edit-produto.componen
 import { NewProdutoComponent } from './pages/new-produto/new-produto.component';
 import { ProdutoFormComponent } from './components/produto-form/produto-form.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     ConfirmDialogComponent,
     EditProdutoComponent,
     NewProdutoComponent,
-    ProdutoFormComponent
+    ProdutoFormComponent,
+    ErrorDialogComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/produtos/produtos.component.ts
+++ src/app/pages/produtos/produtos.component.ts
@@ -3,6 +3,7 @@ import { Produto } from 'src/app/interfaces/Produto';
 import { ProdutosService } from 'src/app/services/produtos.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from 'src/app/components/confirm-dialog/confirm-dialog.component';
+import { ErrorDialogComponent } from 'src/app/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-produtos',
@@ -20,17 +21,26 @@ export class ProdutosComponent implements OnInit {
   ) {}
 
    ngOnInit(): void {
-    this.produtoService.getProdutos().subscribe(data =>this.produtos = data);
+    this.produtoService.getProdutos().subscribe({
+      next: (data) => {
+        this.produtos = data
+      },
+      error: (error) => {
+      this.dialog.open(ErrorDialogComponent, {
+        data: "Error no servidor!"
+      })
+      }
+    });
   }
 
 
   removeProduto(id: number) {
     
-      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      const dialog = this.dialog.open(ConfirmDialogComponent, {
         data: 'Você deseja excluir esse produto?',
       });
   
-      dialogRef.afterClosed().subscribe(result => {  
+      dialog.afterClosed().subscribe(result => {  
         if(result == 'yes' ) {
           this.produtoService.removeProduto(id).subscribe(() => {
             this.produtoService.getProdutos().subscribe(data =>this.produtos = data);
